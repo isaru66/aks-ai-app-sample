476950cadfc7daf9559660ebac832204ec63b52f
Isara Rungvitayakul <isaru_66@hotmail.com>
2026-02-18 15:28:27 +0700

    feat: add support for secret values in Helm template rendering

--- infra/helm/sample-helm-render.sh
+++ infra/helm/sample-helm-render.sh
@@ -2,3 +2,5 @@ helm template ai-app --output-dir ./output
 
 # with values file
 helm template ai-app -f ./ai-app/environments/dev-values.yaml  --output-dir ./output
+
+helm template ai-app -f ./ai-app/environments/dev-values.yaml -f ./ai-app/environments/secret.yaml  --output-dir ./output
