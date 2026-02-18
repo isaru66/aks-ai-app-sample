c107119fc7de22282b1155b59b41e3a3156b7939
Isara Rungvitayakul <isaru_66@hotmail.com>
2026-02-18 17:20:48 +0700

    fix: update sql script

--- scripts/db/init-db.sql
+++ scripts/db/init-db.sql
@@ -1,6 +1,6 @@
 -- Database initialization script for chatapp
 -- This script creates the database, user, and grants necessary privileges
-
+-- run this on database: postgres
 -- Create the database
 CREATE DATABASE chatdb;
 
@@ -14,7 +14,12 @@ GRANT CONNECT ON DATABASE chatdb TO chatapp;
 GRANT ALL PRIVILEGES ON DATABASE chatdb TO chatapp;
 
 -- Note: After connecting to the chatdb database, you may also need to grant schema privileges:
--- \c chatdb
--- GRANT ALL PRIVILEGES ON SCHEMA public TO chatapp;
--- GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO chatapp;
--- GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO chatapp;
+-- run this on database: chatdb
+GRANT ALL PRIVILEGES ON SCHEMA public TO chatapp;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO chatapp;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO chatapp;
+
+GRANT ALL ON SCHEMA public TO chatapp;
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA public
+GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO chatapp;
